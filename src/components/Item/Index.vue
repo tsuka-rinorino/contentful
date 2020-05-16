14994b282c15c8a1515587bbed197e7d1f277d99
RinoTsuka <rino.tsuka.1226@gmail.com>
2020-05-16 21:38:01 +0900

    itemデザイン作成

--- src/components/Item/Index.vue
+++ src/components/Item/Index.vue
@@ -1,23 +1,32 @@
 <template>
-  <div class="item">
+  <article class="item">
     <v-wrap>
-      {{ item.title }}
-      <div v-html="item.content" />
-      <div v-html="item.code" />
+      <h1 class="item__heading">{{ item.title }}</h1>
+      <div class="item__meta">
+        <time class="item__date">{{ item.createdAt | moment("YYYY-MM-DD") }}</time>
+        <v-tag v-if="item.tag[0]" class="item__tag" :tags="item.tag" />
+      </div>
+      <div class="item__body">
+        <div v-html="item.content" />
+        <div v-html="item.code" />
+      </div>
     </v-wrap>
-  </div>
+  </article>
 </template>
+
 
 <script>
 import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
 import MixinContentful from '@/mixins/contentful';
 import client from '@/plugins/contentful';
 import Wrap from '@/components/wrap/index.vue';
+import Tag from '@/components/tag/index.vue';
 
 export default {
   mixins: [MixinContentful],
   components: {
     'v-wrap': Wrap,
+    'v-tag': Tag,
   },
   data() {
     return {
@@ -45,7 +54,51 @@ export default {
 };
 </script>
 
+
 <style lang="scss" scoped>
 .item {
+  margin-top: 5rem;
+
+  &__heading {
+    font-size: 1.375rem;
+    font-weight: 400;
+    text-align: center;
+    line-height: 1.35;
+    margin-top: 0;
+    margin-bottom: 1.5rem;
+  }
+
+  &__meta {
+    text-align: center;
+    margin-bottom: 2.75rem;
+  }
+
+  &__date {
+    display: inline-block;
+    font-size: 0.6875rem;
+    line-height: 1;
+    color: $oc-gray-6;
+    margin-bottom: 0.5rem;
+  }
+
+  &__tag {
+  }
+
+  &__body {
+    &::v-deep {
+      p,
+      ul,
+      ol {
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+      }
+      li {
+        p {
+          margin-top: 0.5rem;
+          margin-bottom: 0.5rem;
+        }
+      }
+    }
+  }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 
+Vue.use(require('vue-moment'));
+
 Vue.config.productionTip = false;
 
 new Vue({
