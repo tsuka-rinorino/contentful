1567366cd075b969eacdce7a1e495a9ece59dd27
RinoTsuka <rino.tsuka.1226@gmail.com>
2020-05-16 13:04:21 +0900

    vue router作成

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="app">
+    <router-view />>
   </div>
 </template>
 
-<script>
-import HelloWorld from '@/components/HelloWorld.vue';
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-};
+<script>
 </script>
 
+
 <style>
-#app {
+.app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
---
+++ src/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="page page--index">
+    <HelloWorld msg="hello"/>
+  </div>
+</template>
+
+
+<script>
+import HelloWorld from '@/components/HelloWorld.vue';
+
+export default {
+  components: {
+    HelloWorld,
+  },
+};
+</script>
+
+
+<style scoped>
+.page--index {
+}
+</style>
