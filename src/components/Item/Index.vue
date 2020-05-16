16f614f26473143b8f8eccbdbd6d57859780d27d
RinoTsuka <rino.tsuka.1226@gmail.com>
2020-05-16 16:25:10 +0900

    整形

--- src/components/Item/Index.vue
+++ src/components/Item/Index.vue
@@ -1,13 +1,18 @@
 <template>
   <div class="item">
     {{ item.title }}
+    <div v-html="item.content" />
+    <div v-html="item.code" />
   </div>
 </template>
 
 <script>
+import { documentToHtmlString } from '@contentful/rich-text-html-renderer';
+import MixinContentful from '@/mixins/contentful';
 import client from '../../plugins/contentful';
 
 export default {
+  mixins: [MixinContentful],
   data() {
     return {
       item: {},
@@ -26,6 +31,8 @@ export default {
           this.item = response.items[0].fields;
           this.item.createdAt = response.items[0].sys.createdAt;
           this.item.updatedAt = response.items[0].sys.updatedAt;
+          this.$set(this.item, 'content', documentToHtmlString(this.item.content));
+          this.$set(this.item, 'code', this.unescapeHTML(documentToHtmlString(this.item.code)));
         });
     },
   },
