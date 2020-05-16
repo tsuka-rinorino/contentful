01323ff8044138f30d4c81720ee7a701e37387f8
RinoTsuka <rino.tsuka.1226@gmail.com>
2020-05-16 20:35:00 +0900

    header作成

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app" class="app">
+    <v-header />
     <router-view />
   </div>
 </template>
 
 
 <script>
+import Header from '@/components/header/index.vue';
+
+export default {
+  components: {
+    'v-header': Header,
+  },
+};
 </script>
 
 
@@ -13,11 +21,6 @@
 @import "@/assets/scss/app.scss";
 
 .app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100%;
 }
 </style>
---
+++ src/components/header/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="header">
+    <h1 class="header__title">{{ title }}</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: process.env.VUE_APP_SITE_TITLE,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  height: 40px;
+
+  &__title {
+    font-size: 14px;
+    font-weight: 400;
+    letter-spacing: 0;
+    margin-top: 0;
+    margin-bottom: 0;
+  }
+}
+</style>
